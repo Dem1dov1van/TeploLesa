1d2f40c9941947f7f64c67787f6cc0f37fcea884
elik <elik@example.com>
2021-05-29 13:46:30 +0300

    pk

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-$('#js-burger').click(function() {
-   $('#list').removeClass('slide');
-   $('#list').toggleClass('visible');
-   $('#popup').removeClass('visible1');
- });
-$('#visible-popup').click(function() {
-   $('#list').toggleClass('slide');
-   $('#popup').toggleClass('visible1');
- });
-$('#back-to-list').click(function() {
-   $('#list').removeClass('slide');
-   $('#popup').toggleClass('visible1');
- });
+// $('#js-burger').click(function() {
+//    $('#list').removeClass('slide');
+//    $('#list').toggleClass('visible');
+//    $('#popup').removeClass('visible1');
+//  });
+// $('#projects-popup').click(function() {
+//    $('#list').toggleClass('slide');
+//    $('#popup').toggleClass('visible1');
+//  });
+
+// $('#services-popup').click(function() {
+//    $('#list').toggleClass('slide');
+//    $('#popup1').toggleClass('visible1');
+//  });
+
+// $('#back-to-list').click(function() {
+//    $('#list').removeClass('slide');
+//    $('#popup').toggleClass('visible1');
+//  });
+// $('#back-to-list1').click(function() {
+//    $('#list').removeClass('slide');
+//    $('#popup1').toggleClass('visible1');
+//  });
 
